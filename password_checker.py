947c99c80755fab1b816308f6f801c10bfeb8a2d
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-29 16:50:08 +0500

    hashing pass and sending request

--- password_checker.py
+++ password_checker.py
@@ -1,5 +1,25 @@
+import hashlib
+
+import requests
+
+
+def request_api_data(query_chars):
+    sha1_hash = hash_pass(query_chars)
+    url = f'https://api.pwnedpasswords.com/range/{sha1_hash[:5]}'
+    res = requests.get(url)
+    print(res, res.status_code)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}')
+    print(res.text)
+
+
+def hash_pass(password):
+    sha1hash = hashlib.sha1(str(password).encode('utf-8')).hexdigest().upper()
+    return sha1hash
+
+
 def main():
-    pass
+    request_api_data('password')
 
 
 if __name__ == '__main__':
